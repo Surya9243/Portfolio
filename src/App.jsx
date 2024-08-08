b9acfcd67d234f9c84175a6d166a373e8b6f8363
Surya9243 <suryadeep374@gmail.com>
2024-08-08 13:18:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <h1>Suryadeep</h1>
+        <marquee behavior="" direction="">Front End Developer</marquee>
+        <div>
+        <img src="https://img.icons8.com/ios-filled/50/FAB005/linkedin.png" alt="linkedin"/>
+        <img  src="https://img.icons8.com/ios-filled/50/FAB005/facebook--v1.png" alt="facebook--v1"/>
+        <img width="100" height="100" src="https://img.icons8.com/carbon-copy/100/FAB005/instagram-new--v1.png" alt="instagram-new--v1"/>
+        <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/FAB005/external-telegram-messenger-with-voice-over-ip-service-logo-bold-tal-revivo.png" alt="external-telegram-messenger-with-voice-over-ip-service-logo-bold-tal-revivo"/>
+        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/FAB005/twitterx--v1.png" alt="twitterx--v1"/>
+        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/FAB005/new-post.png" alt="new-post"/>
+        </div>
+        <div>
+          <div><p>Download CV</p> <p>Contact Me</p></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
